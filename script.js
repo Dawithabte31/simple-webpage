1523473907979928b0a13b047c53b0435639f930
Dawithabte31 <dawithabitamu31@gmail.com>
2025-01-05 23:28:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("actionButton").addEventListener("click", () => {
+    const responseMessage = document.getElementById("responseMessage");
+    responseMessage.style.display = "block";
+});
