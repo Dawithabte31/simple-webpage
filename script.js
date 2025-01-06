f50ed05428b950eea6f7a75687457160b670870d
Dawithabte31 <dawithabitamu31@gmail.com>
2025-01-06 07:59:22 +0300

    files are added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 document.getElementById("actionButton").addEventListener("click", () => {
     const responseMessage = document.getElementById("responseMessage");
     responseMessage.style.display = "block";
